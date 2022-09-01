aec1654ae68141223701343b4cc4ad4c364af0bf
Lindsey Maag <lindseymaag@gmail.comgit config --global user.email lindseymaag@gmail.comgit config --user.email lindseymaag@gmail.com>
2022-09-01 16:25:22 -0500

    SQL backup

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,65 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
+
+-- Use Dictinct with Orderby 
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no)
+    emp_no,
+    first_name,
+    last_name,
+    title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- Retiring Titles Count
+
+-- Mentorship Eligibility 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+
+INTO mentorship_eligibility
+FROM employees AS e
+
+INNER JOIN dept_emp as de 
+ON e.emp_no = de.emp_no
+
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+
+WHERE (de.to_date = '9999-01-01')
+     AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+
+ORDER BY e.emp_no;
+
+
+
